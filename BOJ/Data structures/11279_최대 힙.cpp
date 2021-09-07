25e4afa9ecb7c9405c5ff04776c7d13a8fd936c1
함형진 <73492591+hyungJinn@users.noreply.github.com>
2021-09-07 13:19:42 +0900

    Update 11279_최대 힙.cpp

--- BOJ/Data structures/11279_최대 힙.cpp
+++ BOJ/Data structures/11279_최대 힙.cpp
@@ -6,41 +6,36 @@
 using namespace std;
 
 int N;
-priority_queue<int> q;
+priority_queue<int> pq;
 
-void input() {
+void Solution() {
     cin >> N;
-}
-
-void solution() {
-    int X;
     for (int i = 0; i < N; i++) {
-        cin >> X;
-        if (X == 0) {
-            if (q.size() == 0) {
+        int x; cin >> x;
+        if (x == 0) {
+            if (pq.size() == 0) {
                 cout << 0 << endl;
             }
             else {
-                cout << q.top() << endl;
-                q.pop();
+                cout << pq.top() << endl;
+                pq.pop();
             }
         }
         else {
-            q.push(X);
+            pq.push(x);
         }
     }
 }
 
-void solve() {
-    input();
-    solution();
+void Solve() {
+    Solution();
 }
 
 int main() {
     cin.tie(NULL);
     ios::sync_with_stdio(false);
 
-    solve();
+    Solve();
 
     return 0;
 }
